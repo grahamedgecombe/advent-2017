2a8c0ae9d871e7718a2aaabc3d0662de1052684a
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-02 14:59:01 +0000

    Add day 1 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day1.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day1.java
@@ -5,15 +5,24 @@ import java.io.IOException;
 public final class Day1 {
 	public static void main(String[] args) throws IOException {
 		String list = AdventUtils.readString("day1.txt");
-		System.out.println(sum(list));
+		System.out.println(sumPart1(list));
+		System.out.println(sumPart2(list));
 	}
 
-	public static int sum(String list) {
+	public static int sumPart1(String list) {
+		return sum(list, 1);
+	}
+
+	public static int sumPart2(String list) {
+		return sum(list, list.length() / 2);
+	}
+
+	private static int sum(String list, int steps) {
 		int sum = 0;
 
 		for (int i = 0; i < list.length(); i++) {
 			char c1 = list.charAt(i);
-			char c2 = list.charAt((i + 1) % list.length());
+			char c2 = list.charAt((i + steps) % list.length());
 
 			if (c1 == c2) {
 				sum += c1 - '0';
--- src/test/java/com/grahamedgecombe/advent2017/Day1Test.java
+++ src/test/java/com/grahamedgecombe/advent2017/Day1Test.java
@@ -7,9 +7,18 @@ import static org.junit.Assert.assertEquals;
 public final class Day1Test {
 	@Test
 	public void testPart1() {
-		assertEquals(3, Day1.sum("1122"));
-		assertEquals(4, Day1.sum("1111"));
-		assertEquals(0, Day1.sum("1234"));
-		assertEquals(9, Day1.sum("91212129"));
+		assertEquals(3, Day1.sumPart1("1122"));
+		assertEquals(4, Day1.sumPart1("1111"));
+		assertEquals(0, Day1.sumPart1("1234"));
+		assertEquals(9, Day1.sumPart1("91212129"));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals(6, Day1.sumPart2("1212"));
+		assertEquals(0, Day1.sumPart2("1221"));
+		assertEquals(4, Day1.sumPart2("123425"));
+		assertEquals(12, Day1.sumPart2("123123"));
+		assertEquals(4, Day1.sumPart2("12131415"));
 	}
 }
