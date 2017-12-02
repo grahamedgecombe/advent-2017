d8b747b9ff845c4c6aee3a2c7358925fbdfc0e40
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-02 15:09:14 +0000

    Add day 2 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day2.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day2.java
@@ -1,15 +1,18 @@
 package com.grahamedgecombe.advent2017;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Day2 {
 	public static void main(String[] args) throws IOException {
 		List<String> rows = AdventUtils.readLines("day2.txt");
-		System.out.println(checksum(rows));
+		System.out.println(checksumPart1(rows));
+		System.out.println(checksumPart2(rows));
 	}
 
-	public static int checksum(List<String> rows) {
+	public static int checksumPart1(List<String> rows) {
 		int checksum = 0;
 
 		for (String row : rows) {
@@ -23,6 +26,31 @@ public final class Day2 {
 			}
 
 			checksum += max - min;
+		}
+
+		return checksum;
+	}
+
+	public static int checksumPart2(List<String> rows) {
+		int checksum = 0;
+
+		for (String row : rows) {
+			List<Integer> cols = Arrays.stream(row.split("\t")).map(Integer::parseInt).collect(Collectors.toList());
+
+			next_row: for (int i = 0; i < cols.size(); i++) {
+				for (int j = 0; j < i; j++) {
+					int v1 = cols.get(i);
+					int v2 = cols.get(j);
+
+					if ((v1 % v2) == 0) {
+						checksum += v1 / v2;
+						break next_row;
+					} else if ((v2 % v1) == 0) {
+						checksum += v2 / v1;
+						break next_row;
+					}
+				}
+			}
 		}
 
 		return checksum;
--- src/test/java/com/grahamedgecombe/advent2017/Day2Test.java
+++ src/test/java/com/grahamedgecombe/advent2017/Day2Test.java
@@ -9,6 +9,11 @@ import static org.junit.Assert.assertEquals;
 public final class Day2Test {
 	@Test
 	public void testPart1() {
-		assertEquals(18, Day2.checksum(Arrays.asList("5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8")));
+		assertEquals(18, Day2.checksumPart1(Arrays.asList("5\t1\t9\t5", "7\t5\t3", "2\t4\t6\t8")));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals(9, Day2.checksumPart2(Arrays.asList("5\t9\t2\t8", "9\t4\t7\t3", "3\t8\t6\t5")));
 	}
 }
