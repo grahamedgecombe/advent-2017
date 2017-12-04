0eb921b26338d0bbf98c06f5260d5726d86ac30f
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-04 19:37:39 +0000

    Add day 4 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day4.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day4.java
@@ -2,14 +2,16 @@ package com.grahamedgecombe.advent2017;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Day4 {
 	public static void main(String[] args) throws IOException {
 		List<String> passphrases = AdventUtils.readLines("day4.txt");
-		System.out.println(passphrases.stream().filter(Day4::isValid).count());
+		System.out.println(passphrases.stream().filter(Day4::isValidPart1).count());
+		System.out.println(passphrases.stream().filter(Day4::isValidPart2).count());
 	}
 
-	public static boolean isValid(String passphrase) {
+	public static boolean isValidPart1(String passphrase) {
 		String[] words = passphrase.split(" ");
 		for (int i = 0; i < words.length; i++) {
 			for (int j = 0; j < i; j++) {
@@ -20,5 +22,24 @@ public final class Day4 {
 		}
 
 		return true;
+	}
+
+	public static boolean isValidPart2(String passphrase) {
+		String[] words = passphrase.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (isAnagram(words[i], words[j])) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private static boolean isAnagram(String a, String b) {
+		List<Integer> sortedA = a.chars().sorted().boxed().collect(Collectors.toList());
+		List<Integer> sortedB = b.chars().sorted().boxed().collect(Collectors.toList());
+		return sortedA.equals(sortedB);
 	}
 }
--- src/test/java/com/grahamedgecombe/advent2017/Day4Test.java
+++ src/test/java/com/grahamedgecombe/advent2017/Day4Test.java
@@ -8,8 +8,17 @@ import static org.junit.Assert.assertTrue;
 public final class Day4Test {
 	@Test
 	public void testPart1() {
-		assertTrue(Day4.isValid("aa bb cc dd ee"));
-		assertFalse(Day4.isValid("aa bb cc dd aa"));
-		assertTrue(Day4.isValid("aa bb cc dd aaa"));
+		assertTrue(Day4.isValidPart1("aa bb cc dd ee"));
+		assertFalse(Day4.isValidPart1("aa bb cc dd aa"));
+		assertTrue(Day4.isValidPart1("aa bb cc dd aaa"));
+	}
+
+	@Test
+	public void testPart2() {
+		assertTrue(Day4.isValidPart2("abcde fghij"));
+		assertFalse(Day4.isValidPart2("abcde xyz ecdab"));
+		assertTrue(Day4.isValidPart2("a ab abc abd abf abj"));
+		assertTrue(Day4.isValidPart2("iiii oiii ooii oooi oooo"));
+		assertFalse(Day4.isValidPart2("oiii ioii iioi iiio"));
 	}
 }
