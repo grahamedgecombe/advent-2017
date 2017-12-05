013d43b389095ac1cf7d2326d379d6d7e4d5d90d
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-05 18:27:39 +0000

    Add day 5 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day5.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day5.java
@@ -1,25 +1,38 @@
 package com.grahamedgecombe.advent2017;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public final class Day5 {
 	public static void main(String[] args) throws IOException {
 		List<Integer> offsets = AdventUtils.readLines("day5.txt").stream().map(Integer::parseInt).collect(Collectors.toList());
-		System.out.println(steps(offsets));
+		System.out.println(steps(offsets, Day5::nextOffsetPart1));
+		System.out.println(steps(offsets, Day5::nextOffsetPart2));
 	}
 
-	public static int steps(List<Integer> offsets) {
+	public static int steps(List<Integer> offsets, Function<Integer, Integer> nextOffset) {
+		offsets = new ArrayList<>(offsets);
+
 		int pos = 0, steps = 0;
 
 		while (pos >= 0 && pos < offsets.size()) {
 			int offset = offsets.get(pos);
-			offsets.set(pos, offset + 1);
+			offsets.set(pos, nextOffset.apply(offset));
 			pos += offset;
 			steps++;
 		}
 
 		return steps;
+	}
+
+	public static int nextOffsetPart1(int offset) {
+		return offset + 1;
+	}
+
+	public static int nextOffsetPart2(int offset) {
+		return offset >= 3 ? offset - 1 : offset + 1;
 	}
 }
--- src/test/java/com/grahamedgecombe/advent2017/Day5Test.java
+++ src/test/java/com/grahamedgecombe/advent2017/Day5Test.java
@@ -9,6 +9,11 @@ import static org.junit.Assert.assertEquals;
 public final class Day5Test {
 	@Test
 	public void testPart1() {
-		assertEquals(5, Day5.steps(Arrays.asList(0, 3, 0, 1, -3)));
+		assertEquals(5, Day5.steps(Arrays.asList(0, 3, 0, 1, -3), Day5::nextOffsetPart1));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals(10, Day5.steps(Arrays.asList(0, 3, 0, 1, -3), Day5::nextOffsetPart2));
 	}
 }
