c0f485c63f909c7a1b92886a6e3473c7db7c3032
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-10 10:34:15 +0000

    Add day 10 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day10.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day10.java
@@ -5,26 +5,71 @@ import java.util.Arrays;
 
 public final class Day10 {
 	public static void main(String[] args) throws IOException {
-		int[] lengths = Arrays.stream(AdventUtils.readString("day10.txt").split(","))
+		String in = AdventUtils.readString("day10.txt");
+		int[] lengths = Arrays.stream(in.split(","))
 			.mapToInt(Integer::parseInt)
 			.toArray();
-		System.out.println(knot(256, lengths));
+		System.out.println(knotSingleRound(256, lengths));
+		System.out.println(knotHash(in));
 	}
 
-	public static int knot(int totalLength, int[] lengths) {
+	private static int[] knot(int totalLength, int[] lengths, int rounds) {
 		int[] list = new int[totalLength];
 		for (int i = 0; i < list.length; i++) {
 			list[i] = i;
 		}
 
 		int pos = 0, skip = 0;
-		for (int length : lengths) {
-			reverse(list, pos, length);
-			pos += length + skip;
-			skip++;
+		for (int i = 0; i < rounds; i++) {
+			for (int length : lengths) {
+				reverse(list, pos, length);
+				pos += length + skip;
+				skip++;
+			}
 		}
 
+		return list;
+	}
+
+	public static int knotSingleRound(int totalLength, int[] lengths) {
+		int[] list = knot(totalLength, lengths, 1);
 		return list[0] * list[1];
+	}
+
+	public static String knotHash(String in) {
+		char[] chars = in.toCharArray();
+
+		int[] lengths = new int[chars.length + 5];
+		for (int i = 0; i < chars.length; i++) {
+			lengths[i] = (int) chars[i];
+		}
+
+		lengths[lengths.length - 5] = 17;
+		lengths[lengths.length - 4] = 31;
+		lengths[lengths.length - 3] = 73;
+		lengths[lengths.length - 2] = 47;
+		lengths[lengths.length - 1] = 23;
+
+		int[] sparse = knot(256, lengths, 64);
+		StringBuilder denseHex = new StringBuilder();
+
+		for (int i = 0; i < 16; i++) {
+			int value = sparse[i * 16];
+
+			for (int j = 1; j < 16; j++) {
+				value ^= sparse[i * 16 + j];
+			}
+
+			String hex = Integer.toUnsignedString(value, 16);
+			if (hex.length() == 1) {
+				denseHex.append('0');
+			} else if (hex.length() != 2) {
+				throw new IllegalStateException();
+			}
+			denseHex.append(hex);
+		}
+
+		return denseHex.toString();
 	}
 
 	private static void reverse(int[] list, int pos, int length) {
--- src/test/java/com/grahamedgecombe/advent2017/Day10Test.java
+++ src/test/java/com/grahamedgecombe/advent2017/Day10Test.java
@@ -7,6 +7,14 @@ import static org.junit.Assert.assertEquals;
 public final class Day10Test {
 	@Test
 	public void testPart1() {
-		assertEquals(12, Day10.knot(5, new int[] { 3, 4, 1, 5 }));
+		assertEquals(12, Day10.knotSingleRound(5, new int[] { 3, 4, 1, 5 }));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals("a2582a3a0e66e6e86e3812dcb672a272", Day10.knotHash(""));
+		assertEquals("33efeb34ea91902bb2f59c9920caa6cd", Day10.knotHash("AoC 2017"));
+		assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", Day10.knotHash("1,2,3"));
+		assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", Day10.knotHash("1,2,4"));
 	}
 }
