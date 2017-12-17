2191aa58852ab361ae260255987e948999101deb
Graham Edgecombe <gpe@grahamedgecombe.com>
2017-12-17 10:56:36 +0000

    Add day 17 part 2 solution

--- src/main/java/com/grahamedgecombe/advent2017/Day17.java
+++ src/main/java/com/grahamedgecombe/advent2017/Day17.java
@@ -8,6 +8,7 @@ public final class Day17 {
 	public static void main(String[] args) throws IOException {
 		int steps = Integer.parseInt(AdventUtils.readString("day17.txt"));
 		System.out.println(getPart1(steps));
+		System.out.println(getPart2(steps));
 	}
 
 	public static int getPart1(int steps) {
@@ -22,5 +23,21 @@ public final class Day17 {
 		}
 
 		return ring.get((pos + 1) % ring.size());
+	}
+
+	public static int getPart2(int steps) {
+		int value = 0;
+
+		int pos = 0, size = 1;
+		for (int i = 1; i <= 50000000; i++) {
+			pos = (pos + steps) % size;
+			if (pos == 0) {
+				value = i;
+			}
+			size++;
+			pos = (pos + 1) % size;
+		}
+
+		return value;
 	}
 }
